a21f983ddf6c99e92a9a3e5270e5d167fe69d233
Nghia Phan <phanvannghia17062002@gmail.com>
2025-01-07 08:31:58 +0700

    home

--- shoe/src/layout/layoutDefault/index.jsx
+++ shoe/src/layout/layoutDefault/index.jsx
@@ -2,14 +2,18 @@ import Header from './Header'
 import Footer from './Footer'
 import React from 'react'
 import "./style.scss"
+import { Outlet } from 'react-router-dom'
 
 const LayoutDefault = () => {
-  return (
-	<>
-	 <Header/>
-	 <Footer/>
-	</>
-  )
+	return (
+		<>
+			<Header />
+			<main className='layout-default__main'>
+				<Outlet />
+			</main>
+			<Footer />
+		</>
+	)
 }
 
 export default LayoutDefault
--- shoe/src/page/Client/Home/index.jsx
+++ shoe/src/page/Client/Home/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { Carousel } from 'antd';
+import './style.scss'
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [dotPosition, setDotPosition] = useState('bottom');
+    return (
+        <>
+            <Carousel autoplay autoplaySpeed={10000}  arrows dotPosition={dotPosition} className='casuael'>
+                <div className='group-image'>
+                    <img src="https://img1-cdn.halftime.pt/2022/04/11/e/8/e8f112f491873ac9.jpg" alt="" className='group-image__item' />
+                </div>
+                <div className='group-image'>
+                    <img src="https://authentic-shoes.com/wp-content/uploads/2023/09/image-54-2048x711-2.webp" alt="" className='group-image__item'/>
+                </div>
+                <div className='group-image'>
+                    <img src="https://authentic-shoes.com/wp-content/uploads/2023/09/image-54-2048x711-2.webp" alt="" className='group-image__item'/>
+                </div>
+            </Carousel>
+        </>
+    )
 }
 
 export default Home
